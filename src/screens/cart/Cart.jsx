16de71a7210aa19017b9e867f92711695b525d29
Rostyslav Klym <rostik.klym@gmail.com>
2023-05-28 11:56:48 +0300

    added Cart display

--- src/screens/cart/Cart.jsx
+++ src/screens/cart/Cart.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoodCard from "../../components/goodCard";
 
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  cards_container: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+  },
+});
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const [goodsData, setGoodsData] = useState([]);
+  const styles = useStyles();
+
+  useEffect(() => {
+    const getGoods = () => {
+      setGoodsData(JSON.parse(localStorage.getItem("cart")));
+    };
+
+    getGoods();
+  }, []);
+
+  return (
+    <ul className={styles.cards_container}>
+      {goodsData.map((product) => (
+        <GoodCard key={product.id} product={product} />
+      ))}
+    </ul>
+  );
 };
 
 export default Cart;
