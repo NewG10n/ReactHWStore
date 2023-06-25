ec0597b6e227341b11c9e203d31b93e2cd813db1
Rostyslav Klym <rostik.klym@gmail.com>
2023-06-25 19:19:06 +0300

    fixed checked status

--- src/components/viewToggle/ViewToggle.jsx
+++ src/components/viewToggle/ViewToggle.jsx
@@ -3,12 +3,15 @@ import { ViewContext } from "../../App";
 export const ViewToggle = () => {
   const { view, setView } = useContext(ViewContext);
 
+  const handleChange = () => setView(view === "tiles" ? "list" : "tiles");
+
   return (
     <>
       <input
         type="checkbox"
+        checked={view === "tiles" ? false : true}
+        onChange={handleChange}
         id="switch"
-        onChange={() => setView(view === "tiles" ? "list" : "tiles")}
       />
       <label htmlFor="switch">List view</label>
     </>
